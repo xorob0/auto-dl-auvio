42b2075a23a07f10aef56e511c2b4fa3a25ea319
Tim Simon <tim@mirahi.io>
2021-02-01 17:10:53 +0100

    optimize chrome preferences

--- main.py
+++ main.py
@@ -9,18 +9,19 @@ import time
 
 showName = re.split('\?', re.split('_', sys.argv[1])[1])[0]
 
-while True:
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--window-size=1366x768")
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_prefs = {}
-    chrome_options.experimental_options["prefs"] = chrome_prefs
-    chrome_prefs["profile.default_content_settings"] = {"images": 2}
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1366x768")
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_prefs = {}
+chrome_options.experimental_options["prefs"] = chrome_prefs
+chrome_prefs["profile.default_content_settings"] = {"images": 2}
 
+
+while True:
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(sys.argv[1])
 
